2a9169135fdb0006d5fb34bf412cea88fb8689bf
jhoey <jhoey@magicwrighter.com>
2022-03-31 19:24:07 -0400

    First commit of skelton PoC

---
+++ MagicAPI/Form1.cs
@@ -0,0 +1,246 @@
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.Logging;
+using static MagicAPI.Models;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+
+namespace MagicAPI
+{
+	public partial class Form1 : Form
+	{
+		static HttpClient client = new HttpClient();
+		public Dictionary<string, PictureBox> pictureBoxes;
+		public string clickedPicture;
+		public PictureBox clickedBox;
+		public string fPath;
+		public Dictionary<string, Card> cardCache = new Dictionary<string, Card>();
+		
+
+		public Form1()
+		{
+			
+			InitializeComponent();
+			label1.Visible = false;
+			label2.Visible = false;
+			label3.Visible = false;
+			textBox1.Visible = false;
+			textBox2.Visible = false;
+			richTextBox1.Visible = false;
+
+			// Try to create the directory.
+			dw("Creating temporary directory if none exists.");
+				DirectoryInfo di = Directory.CreateDirectory("tmp");
+				Debug.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime("tmp"));
+				fPath = $"{Directory.GetCurrentDirectory()}\\tmp\\";
+		}
+
+		async Task<Card> GetCardAsync(string path)
+		{
+			Card card = null;
+			dw("Attempting to GET card from ScryfallAPI 'Random' endpoint.");
+			HttpResponseMessage response = await client.GetAsync(path);
+			if (response.IsSuccessStatusCode)
+			{
+				dw("received success status code");
+				card = await response.Content.ReadAsAsync<Card>();
+				Task.WaitAll();
+				if (card.ImageUris is null)
+				{
+					dw("card.ImageUris is null, recurring method.");
+					return await GetCardAsync(path);
+				}
+				if (cardCache.ContainsKey(card.OracleId))
+				{
+					dw("card already exists, recurring method.");
+					return await GetCardAsync(path);
+				}
+				dw("card is valid. returning card.");
+				return card;
+			}
+			dw("Received failure code. recurring method.");
+			return await GetCardAsync(path);
+		}
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			var url = "https://api.scryfall.com/cards/random";
+			pictureBoxes = new Dictionary<string, PictureBox>();
+			pictureBoxes.Add("pictureBox1", pictureBox1);
+			pictureBoxes.Add("pictureBox2", pictureBox2);
+			pictureBoxes.Add("pictureBox3", pictureBox3);
+			pictureBoxes.Add("pictureBox4", pictureBox4);
+			pictureBoxes.Add("pictureBox5", pictureBox5);
+			pictureBoxes.Add("pictureBox6", pictureBox6);
+			pictureBoxes.Add("pictureBox7", pictureBox7);
+
+			foreach (var pb in pictureBoxes)
+			{
+				pb.Value.ImageLocation = null;
+
+				while (pb.Value.ImageLocation is null)
+				{
+					Card card = await GetCardAsync(url);
+					
+					cacheImage(card.ImageUris.Normal, card.OracleId);
+
+						card.imageSrc = $"{Directory.GetCurrentDirectory()}\\tmp\\{card.OracleId}.png";
+						card.pictureBoxName = pb.Value.Name;
+						cacheCard(pb.Value.Name, card);
+					pb.Value.ImageLocation = card.imageSrc;
+				}
+			}
+		}
+
+		private void pictureBox_MouseHover(object sender, EventArgs e)
+		{
+			PictureBox pb = sender as PictureBox;
+			if (pb != null)
+			{
+				pictureBox8.ImageLocation = pb.ImageLocation;
+			}
+		}
+
+		private void pictureBox_HoverOff(object sender, EventArgs e)
+		{
+			PictureBox pb = sender as PictureBox;
+			pictureBox8.ImageLocation = null;
+			Card cardPick = cardCache.GetValueOrDefault(pb.Name);
+			textBox1.Visible = false;
+			textBox2.Visible = false;
+			richTextBox1.Visible = false;
+			label1.Visible = false;
+			label2.Visible = false;
+			label3.Visible = false;
+			textBox1.Text = "";
+			textBox2.Text = "";
+			richTextBox1.Text = "";
+		}
+
+		private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+		{
+			PictureBox pb = sender as PictureBox;
+			if (clickedPicture == pb.Name)
+			{
+				dw("clickedPicture == pb.Name");
+				unselectPictureBox(pb);
+				clickedPicture = null;
+				clickedBox = null;
+			}
+			if (clickedPicture != pb.Name)
+			{
+				dw("clickedPicture is not equal to pb.Name");
+				dw("unselecting clickedbox");
+				unselectPictureBox(clickedBox);
+				clickedPicture = pb.Name;
+				clickedBox = pb;
+				selectPictureBox(pb);
+
+				
+			}
+			if (clickedPicture is null)
+			{
+				dw("clickedPicture is null");
+				clickedPicture = pb.Name;
+				selectPictureBox(pb);
+				clickedBox = pb;
+				//string name = Regex.Replace(pb.Value.ImageLocation, @"(?<=\\)[^\\]*\.png", "");
+				//textBox1.Text = cardCache.First(p => p.OracleId == name).ToString();
+			}
+			
+			
+		}
+
+		private void unselectPictureBox(PictureBox pb)
+		{
+			foreach (var VARIABLE in pictureBoxes)
+			{
+				VARIABLE.Value.MouseHover += pictureBox_MouseHover;
+				VARIABLE.Value.MouseLeave += pictureBox_HoverOff;
+			}
+
+			pb.BorderStyle = BorderStyle.None;
+			pb.Width = 200;
+			pb.Height = 300;
+			pb.Left += 15;
+			pb.Top += 40;
+			clickedBox = null;
+		}
+
+		private void selectPictureBox(PictureBox pb)
+		{
+			foreach (var VARIABLE in pictureBoxes)
+			{
+				VARIABLE.Value.MouseHover -= pictureBox_MouseHover;
+				VARIABLE.Value.MouseLeave -= pictureBox_HoverOff;
+			}
+
+			pb.BorderStyle = BorderStyle.Fixed3D;
+			pb.Width = 230;
+			pb.Height = 330;
+			pb.Left -= 15;
+			pb.Top -= 40;
+			pictureBox8.ImageLocation = pb.ImageLocation;
+			Card cardPick = cardCache.GetValueOrDefault(pb.Name);
+			textBox1.Visible = true;
+			textBox2.Visible = true;
+			richTextBox1.Visible = true;
+			label1.Visible = true;
+			label2.Visible = true;
+			label3.Visible = true;
+			textBox1.Text = cardPick.Name;
+			textBox2.Text = cardPick.ManaCost;
+			richTextBox1.Text = cardPick.OracleText;
+		}
+
+		public void cacheImage(string url, string name)
+		{
+			using (WebClient webClient = new WebClient())
+			{
+				byte[] data = webClient.DownloadData(url);
+
+				using (MemoryStream mem = new MemoryStream(data))
+				{
+					using (var yourImage = Image.FromStream(mem))
+					{
+						string fPName = $"{fPath}{name}.png";
+						// If you want it as Png
+						yourImage.Save(fPName, ImageFormat.Png);
+					}
+				}
+
+			}
+		}
+
+		private void cacheCard(string pbName, Card card)
+		{
+			cardCache.Add(card.pictureBoxName, card);
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			clearTempFiles($"{Directory.GetCurrentDirectory()}\\tmp\\");
+		}
+
+		private void clearTempFiles(string targetDirectory)
+		{
+			string[] fileEntries = Directory.GetFiles(targetDirectory);
+			foreach (string fileName in fileEntries)
+			{
+				File.Delete(fileName);
+			}
+				
+		}
+
+		public void dw(string message)
+		{
+			Debug.WriteLine(message);
+		}
+	}
+}
---
+++ MagicAPI/Models.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MagicAPI
+{
+	public class Models
+	{
+		public class ImageUris
+		{
+			[JsonProperty("small")] public string Small;
+			[JsonProperty("normal")] public string Normal;
+			[JsonProperty("large")] public string Large;
+			[JsonProperty("png")] public string Png;
+			[JsonProperty("art_crop")] public string ArtCrop;
+			[JsonProperty("border_crop")] public string BorderCrop;
+		}
+
+		public class Legalities
+		{
+			[JsonProperty("standard")] public string Standard;
+			[JsonProperty("future")] public string Future;
+			[JsonProperty("historic")] public string Historic;
+			[JsonProperty("gladiator")] public string Gladiator;
+			[JsonProperty("pioneer")] public string Pioneer;
+			[JsonProperty("modern")] public string Modern;
+			[JsonProperty("legacy")] public string Legacy;
+			[JsonProperty("pauper")] public string Pauper;
+			[JsonProperty("vintage")] public string Vintage;
+			[JsonProperty("penny")] public string Penny;
+			[JsonProperty("commander")] public string Commander;
+			[JsonProperty("brawl")] public string Brawl;
+			[JsonProperty("historicbrawl")] public string Historicbrawl;
+			[JsonProperty("alchemy")] public string Alchemy;
+			[JsonProperty("paupercommander")] public string Paupercommander;
+			[JsonProperty("duel")] public string Duel;
+			[JsonProperty("oldschool")] public string Oldschool;
+			[JsonProperty("premodern")] public string Premodern;
+		}
+
+		public class Prices
+		{
+			[JsonProperty("usd")] public string Usd;
+			[JsonProperty("usd_foil")] public object UsdFoil;
+			[JsonProperty("usd_etched")] public object UsdEtched;
+			[JsonProperty("eur")] public string Eur;
+			[JsonProperty("eur_foil")] public object EurFoil;
+			[JsonProperty("tix")] public object Tix;
+		}
+
+		public class RelatedUris
+		{
+			[JsonProperty("gatherer")] public string Gatherer;
+			[JsonProperty("tcgplayer_infinite_articles")] public string TcgplayerInfiniteArticles;
+			[JsonProperty("tcgplayer_infinite_decks")] public string TcgplayerInfiniteDecks;
+			[JsonProperty("edhrec")] public string Edhrec;
+			[JsonProperty("mtgtop8")] public string Mtgtop8;
+		}
+
+		public class PurchaseUris
+		{
+			[JsonProperty("tcgplayer")] public string Tcgplayer;
+			[JsonProperty("cardmarket")] public string Cardmarket;
+			[JsonProperty("cardhoarder")] public string Cardhoarder;
+		}
+
+		public class Card
+		{
+			[JsonProperty("object")] public string Object;
+			[JsonProperty("id")] public string Id;
+			[JsonProperty("oracle_id")] public string OracleId;
+			[JsonProperty("multiverse_ids")] public List<int> MultiverseIds;
+			[JsonProperty("tcgplayer_id")] public int TcgplayerId;
+			[JsonProperty("cardmarket_id")] public int CardmarketId;
+			[JsonProperty("name")] public string Name;
+			[JsonProperty("lang")] public string Lang;
+			[JsonProperty("released_at")] public string ReleasedAt;
+			[JsonProperty("uri")] public string Uri;
+			[JsonProperty("scryfall_uri")] public string ScryfallUri;
+			[JsonProperty("layout")] public string Layout;
+			[JsonProperty("highres_image")] public bool HighresImage;
+			[JsonProperty("image_status")] public string ImageStatus;
+			[JsonProperty("image_uris")] public ImageUris ImageUris;
+			[JsonProperty("mana_cost")] public string ManaCost;
+			[JsonProperty("cmc")] public double Cmc;
+			[JsonProperty("type_line")] public string TypeLine;
+			[JsonProperty("oracle_text")] public string OracleText;
+			[JsonProperty("power")] public string Power;
+			[JsonProperty("toughness")] public string Toughness;
+			[JsonProperty("colors")] public List<string> Colors;
+			[JsonProperty("color_identity")] public List<string> ColorIdentity;
+			[JsonProperty("keywords")] public List<object> Keywords;
+			[JsonProperty("legalities")] public Legalities Legalities;
+			[JsonProperty("games")] public List<string> Games;
+			[JsonProperty("reserved")] public bool Reserved;
+			[JsonProperty("foil")] public bool Foil;
+			[JsonProperty("nonfoil")] public bool Nonfoil;
+			[JsonProperty("finishes")] public List<string> Finishes;
+			[JsonProperty("oversized")] public bool Oversized;
+			[JsonProperty("promo")] public bool Promo;
+			[JsonProperty("reprint")] public bool Reprint;
+			[JsonProperty("variation")] public bool Variation;
+			[JsonProperty("set_id")] public string SetId;
+			[JsonProperty("set")] public string Set;
+			[JsonProperty("set_name")] public string SetName;
+			[JsonProperty("set_type")] public string SetType;
+			[JsonProperty("set_uri")] public string SetUri;
+			[JsonProperty("set_search_uri")] public string SetSearchUri;
+			[JsonProperty("scryfall_set_uri")] public string ScryfallSetUri;
+			[JsonProperty("rulings_uri")] public string RulingsUri;
+			[JsonProperty("prints_search_uri")] public string PrintsSearchUri;
+			[JsonProperty("collector_number")] public string CollectorNumber;
+			[JsonProperty("digital")] public bool Digital;
+			[JsonProperty("rarity")] public string Rarity;
+			[JsonProperty("card_back_id")] public string CardBackId;
+			[JsonProperty("artist")] public string Artist;
+			[JsonProperty("artist_ids")] public List<string> ArtistIds;
+			[JsonProperty("illustration_id")] public string IllustrationId;
+			[JsonProperty("border_color")] public string BorderColor;
+			[JsonProperty("frame")] public string Frame;
+			[JsonProperty("full_art")] public bool FullArt;
+			[JsonProperty("textless")] public bool Textless;
+			[JsonProperty("booster")] public bool Booster;
+			[JsonProperty("story_spotlight")] public bool StorySpotlight;
+			[JsonProperty("edhrec_rank")] public int EdhrecRank;
+			[JsonProperty("prices")] public Prices Prices;
+			[JsonProperty("related_uris")] public RelatedUris RelatedUris;
+			[JsonProperty("purchase_uris")] public PurchaseUris PurchaseUris;
+			public string pictureBoxName;
+			public string imageSrc;
+		}
+	}
+}
